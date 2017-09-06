73da587d00b033d114329d5f4d95564e0a2e8ec3
lonny <xingchenyekong@163.com>
2017-09-06 18:48:42 +0800

    change

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
 $str = file_get_contents('php://input');
-log($str);
+saveLog($str);
 
-function log($str = '') {
+function saveLog($str = '') {
 	$handle = fopen('log.txt', 'a');
 
 	$str .= date("Y-m-d H:i:s") . ": ";
