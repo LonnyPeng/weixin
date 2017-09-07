dd98beb45ed80de49287d0f8a30f40d9e9d49fd6
lonny <xingchenyekong@163.com>
2017-09-07 09:32:38 +0800

    change

--- index.php
+++ index.php
@@ -27,6 +27,19 @@ if (isset($_GET["echostr"]) && isset($_GET["signature"]) && isset($_GET["timesta
 	$toUsername = $postObj->ToUserName;
 	$keyword = trim($postObj->Content);
 
+	$textTpl = "<xml>
+<ToUserName><![CDATA[%s]]></ToUserName>
+<FromUserName><![CDATA[%s]]></FromUserName>
+<CreateTime>%s</CreateTime>
+<MsgType><![CDATA[%s]]></MsgType>
+<Content><![CDATA[%s]]></Content>
+<FuncFlag>0</FuncFlag>
+</xml>";
+	
+	$contentStr = sprintf($textTpl, $fromUsername, $toUsername, time(), 'text', $keyword);
+
+	echo $contentStr;
+
 	$handle = fopen('log.txt', 'a');
 
 	$postStr = date("Y-m-d H:i:s") . ": " . $postStr . "\n\r";
