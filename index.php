53585c4751603f62b8bf521ba94bc443805847a6
lonny <xingchenyekong@163.com>
2017-09-07 09:16:18 +0800

    change

--- index.php
+++ index.php
@@ -2,46 +2,32 @@
 
 define("TOKEN", "weixin");
 
-if(checkSignature()) {
-	if (isset($_GET["echostr"])) {
-		echo $_GET["echostr"];
+if (isset($_GET["echostr"]) && isset($_GET["signature"]) && isset($_GET["timestamp"]) && isset($_GET["nonce"])) {
+	$signature = $_GET["signature"];
+	$timestamp = $_GET["timestamp"];
+	$nonce = $_GET["nonce"];
+
+	$token = TOKEN;
+	
+	$tmpArr = array($token, $timestamp, $nonce);
+	sort($tmpArr, SORT_STRING);
+	
+	$tmpStr = implode($tmpArr);
+	$tmpStr = sha1($tmpStr);
+
+	if($tmpStr == $signature) {
+	    echo $_GET["echostr"]
 	} else {
-		echo 'error';
+	    echo 'error';
 	}
-} else{
-    $str = file_get_contents('php://input');
+} else {
+	$str = file_get_contents('php://input');
 
-    $handle = fopen('log.txt', 'a');
+	$handle = fopen('log.txt', 'a');
 
-    $str = date("Y-m-d H:i:s") . ": " . $str . "\n\r";
+	$str = date("Y-m-d H:i:s") . ": " . $str . "\n\r";
 
-    fwrite($handle, $str);
+	fwrite($handle, $str);
 
-    fclose($handle);
-}
-
-function checkSignature()
-{
-	if (!isset($_GET["signature"]) || !isset($_GET["timestamp"]) || !isset($_GET["nonce"])) {
-		return false;
-	}
-    //从GET参数中读取三个字段的值
-    $signature = $_GET["signature"];
-    $timestamp = $_GET["timestamp"];
-    $nonce = $_GET["nonce"];
-    //读取预定义的TOKEN
-    $token = TOKEN;
-    //对数组进行排序
-    $tmpArr = array($token, $timestamp, $nonce);
-    sort($tmpArr, SORT_STRING);
-    //对三个字段进行sha1运算
-    $tmpStr = implode( $tmpArr );
-    $tmpStr = sha1( $tmpStr );
-    //判断我方计算的结果是否和微信端计算的结果相符
-    //这样利用只有微信端和我方了解的token作对比,验证访问是否来自微信官方.
-    if( $tmpStr == $signature ){
-        return true;
-    }else{
-        return false;
-    }
+	fclose($handle);
 }
