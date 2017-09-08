5d23f83af903965c17089a5e4005b436144c9d14
lonny <xingchenyekong@163.com>
2017-09-08 09:21:05 +0800

    cahnge

--- index.php
+++ index.php
@@ -31,7 +31,23 @@ if (isset($_GET["echostr"]) && isset($_GET["signature"]) && isset($_GET["timesta
 	$toUsername = $postObj->ToUserName;
 	$keyword = trim($postObj->Content);
 
-	$result = translateGoogleApi(array('tl' => 'en', 'text' => $keyword));
+	$langArr = array(
+	    "sq", "ar", "am", "az", "ga", "et", "eu", "be", "bg", "is", "pl", "bs", "fa", "af", "da", "de", "ru", "fr", "tl", "fi", 
+	    "fy", "km", "ka", "gu", "kk", "ht", "ko", "ha", "nl", "ky", "gl", "ca", "cs", "kn", "co", "hr", "ku", "la", "lv", "lo", 
+	    "lt", "lb", "ro", "mg", "mt", "mr", "ml", "ms", "mk", "mi", "mn", "bn", "my", "hmn", "xh", "zu", "ne", "no", "pa", "pt", 
+	    "ps", "ny", "ja", "sv", "sm", "sr", "st", "si", "eo", "sk", "sl", "sw", "gd", "ceb", "so", "tg", "te", "ta", "th", "tr", 
+	    "cy", "ur", "uk", "uz", "es", "iw", "el", "haw", "sd", "hu", "sn", "hy", "ig", "it", "yi", "hi", "su", "id", "jw", "en", 
+	    "yo", "vi", "zh-TW", "zh-CN", 
+	);
+
+	$result = explode("@", $keyword);
+	if (count($result) < 2) {
+		$result = "你好，我的世界。@en";
+	} elseif (!in_array($result[1], $langArr)) {
+		$result = sprintf("你好，我的世界。@[%s]", implode(", ", $langArr));
+	} else {
+		$result = translateGoogleApi(array('tl' => $result[1], 'text' => $result[0]));
+	}
 
 	$textTpl = "<xml>
 <ToUserName><![CDATA[%s]]></ToUserName>
